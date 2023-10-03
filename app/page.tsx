2e33d6ca1b6992e4f1bc5d8b77eee3ef9d3df515
ciza Wells <cizawells70@gmail.com>
2023-10-03 12:01:30 +0300

    changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,17 @@
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
+    <div className="">
+
     <p className="text-3xl font-medium text-sky-700">
       Hello world
-    </p>
+      </p>
+      
+      <Button>
+    Click me
+      </Button>
+
+    </div>
   )
 }
