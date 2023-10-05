64b5fff48aa213a3d9fec96fe2f6457fe04b76d7
ciza Wells <cizawells70@gmail.com>
2023-10-05 12:50:22 +0300

    added authentication

---
+++ app/(dashboard)/(routes)/page.tsx
@@ -0,0 +1,13 @@
+import { UserButton } from "@clerk/nextjs";
+
+export default function Home() {
+  return (
+    <p>
+      <UserButton
+        afterSignOutUrl="/"
+        
+
+      />
+   </p>
+  )
+}
