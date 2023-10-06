80f57a88d3dd2c0eba081b6901691fc40a84899e
ciza Wells <cizawells70@gmail.com>
2023-10-06 16:47:32 +0300

    finished authentication and layout settings

---
+++ app/(dashboard)/_components/logo.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image"
+
+
+const Logo = () => {
+  return (
+      <Image
+          height={130}
+          width={130}
+          alt="Logo"
+          src="/logo.svg"
+      />
+  )
+}
+
+export default Logo
