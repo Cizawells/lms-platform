6cef7f34a534edfe9ac244a63ceb967025d2af4e
ciza Wells <cizawells70@gmail.com>
2023-10-20 10:19:56 +0300

    finished publishing chapter api

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/_components/chapter-actions.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/_components/chapter-actions.tsx
@@ -24,6 +24,25 @@ const ChapterActions = ({
   const router = useRouter()
   const [isLoading, setIsLoading] = useState(false)
 
+  const onClick = async () => {
+    try {
+      setIsLoading(true);
+
+      if (isPublished) {
+        await axios.patch(`/api/courses/${courseId}/chapters/${chapterId}/unpublish`);
+        toast.success("Chapter Unpublished");
+      } else {
+        await axios.patch(`/api/courses/${courseId}/chapters/${chapterId}/publish`)
+        toast.success("Chapter Published");
+      }
+
+      router.refresh()
+    } catch (error) {
+      toast.error("Something went wrong")
+    } finally {
+      setIsLoading(false)
+    }
+  }
   const onDelete = async () => {
     try {
       setIsLoading(true);
@@ -41,7 +60,7 @@ const ChapterActions = ({
   return (
       <div className="flex items-center gap-x-2">
           <Button
-              onClick={() => { }}
+              onClick={onClick}
               disabled={disabled}
               variant="outline"
             size="sm">
