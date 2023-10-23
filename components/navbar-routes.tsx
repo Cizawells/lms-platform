640a167949ce11c2c2e3639fbe103bb8fdccf17e
ciza Wells <cizawells70@gmail.com>
2023-10-23 17:58:55 +0300

    working

--- components/navbar-routes.tsx
+++ components/navbar-routes.tsx
@@ -4,6 +4,7 @@ import { UserButton } from "@clerk/nextjs";
 import { LogOut } from "lucide-react";
 import Link from "next/link";
 import { usePathname, useRouter } from "next/navigation";
+import SearchInput from "./search-input";
 import { Button } from "./ui/button";
 
 
@@ -12,8 +13,15 @@ const NavbarRoutes = () => {
     const router = useRouter()
     
     const isTeacherPage = pathName?.startsWith("/teacher");
-    const isPlayerPage = pathName?.includes("/chapter")
-  return (
+    const isPlayerPage = pathName?.includes("/chapter");
+    const isSearchPage = pathName === "/search"
+    return (
+      <>
+            {isSearchPage && (
+                <div className="hidden md:block">
+                    <SearchInput />
+                </div>
+      )}
       <div className="flex gap-x-2 ml-auto">
           {
               isTeacherPage || isPlayerPage ? (
@@ -30,6 +38,7 @@ const NavbarRoutes = () => {
           }
           <UserButton afterSignOutUrl="/"/>
     </div>
+      </>
   )
 }
 
