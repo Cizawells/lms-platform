c77d82fe132c9d6ca8341c6efcb1ce18c8e61ca7
ciza Wells <cizawells70@gmail.com>
2023-10-26 19:50:49 +0300

    finished get chapters function

--- app/(course)/courses/[courseId]/chapters/[chapterId]/page.tsx
+++ app/(course)/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -1,5 +1,16 @@
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 
-const ChapterIdPage = () => {
+const ChapterIdPage = ({
+  params
+}: {
+  params: { courseId: string; chapterId: string}
+  }) => {
+  const { userId } = auth();
+
+  if (!userId) {
+    return redirect("/")
+  }
   return (
     <div>ChapterIdPage</div>
   )
